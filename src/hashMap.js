274c2693d23f4333a2b3433cf2e3c56aea6a8f2e
Daniel <dabies11@gmail.com>
2025-02-01 19:45:26 -0700

    Final commit
    Once I was able to make the array of arrays, things were much more
    straightforward from there. Finished project, and all comments are
    added. On to the next!

--- src/hashMap.js
+++ src/hashMap.js
@@ -1,13 +1,20 @@
-import { Node, LinkedList } from './linkedList'
-
 class HashMap {
-    constructor(initialCapacity = 16; loadFactor = 0.75) {
+    constructor() {
+        //initial variables given by Odin
+        let initialCapacity = 16;
+        const loadFactor = 0.75;
+
+        //initializing all variables
         this.loadFactor = loadFactor;
         this.capacity = initialCapacity;
-        this.buckets = Array.from ({ length: this.capacity }, () => []);
+        //cool function here. array.from() takes an iterable, in this case the length of our capacity, and does something
+        //for that amount of time. in our case, it is returning an empty array, so we will have an array of arrays :D
+        this.buckets = Array.from({ length: this.capacity }, () => []);
         this.size = 0;
     }
 
+    //hashing function provided by Odin. Added in a modular that will divide by the bucket capacity, which will
+    //help with achieving a more even spread
     hash(key) {
         let hashCode = 0;
         const primeNumber = 31;
@@ -18,12 +25,16 @@ class HashMap {
         return hashCode;
     }
 
+    //function for adding a key, value pair into the hashmap
     set(key, value) {
+        //hash the key to determine the index of the bucket it will be going into
         let bucketIndex = this.hash(key);
-        if (bucketIndex < 0 || bucketIndex >= buckets.length) {
+        //throw an error if that bucket index isn't in the array
+        if (bucketIndex < 0 || bucketIndex >= this.buckets.length) {
             throw new Error("Trying to access index out of bounds");
           }
         
+        //if the key is already in our hashmap, we simply update the value and return
         const bucket = this.buckets[bucketIndex];
         for(let entry of bucket) {
             if (entry.key === key) {
@@ -31,11 +42,12 @@ class HashMap {
                 return;
             }
         }
-
+        //add key value pair to our bucket array if it is new, growing the size by one
         bucket.push({ key, value });
         this.size++;
 
-        if(this.size/this.capacity >= this.loadFactor) {
+        //check to see if adding this key pushed us past our load capacity. if it did, we must grow our bucket array
+        if(this.size/this.capacity > this.loadFactor) {
             this.grow();
         }
     }
@@ -45,7 +57,7 @@ class HashMap {
         //set size to zero, (it will go back to normal in next step)
         let oldBuckets = this.buckets;
         this.capacity *= 2;
-        this.buckets = Array.from({ length: this.capacity } () => []);
+        this.buckets = Array.from({ length: this.capacity }, () => []);
         this.size = 0;
 
         //rehash all old entries into the new buckets
@@ -56,10 +68,18 @@ class HashMap {
         }
     }
 
+    //function to return the value associated with a key
     get(key) {
+        //variables to determine what bucket the key is in
         let bucketIndex = this.hash(key);
+        //throw an error if that bucket index isn't in the array
+        if (bucketIndex < 0 || bucketIndex >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+          }
         let bucket = this.buckets[bucketIndex];
 
+        //iterate over every key in that bucket until we find the one we're looking for, and return that value
+        //return null if key isn't found
         for(const entry of bucket) {
             if(entry.key === key) {
                 return entry.value;
@@ -74,36 +94,75 @@ class HashMap {
         //if it does equal null, it returns false
         return this.get(key) !== null? true: false;
     }
-
+    //function to remove a key from hashmap
     remove(key) {
+        //variables to determine what bucket the entry is in, and then what index in that bucket the key is
         let bucketIndex = this.hash(key);
+        //throw an error if that bucket index isn't in the array
+        if (bucketIndex < 0 || bucketIndex >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+          }
         let bucket = this.buckets[bucketIndex];
+        let index = bucket.findIndex((entry) => entry.key === key);
 
-        if(entry) {
-            this.buckets[bucketIndex].
+        //find index returns -1 if the key is not found, so it returns false in that case
+        if (index === -1) {
+            return false;
+        } else {
+            //if index is found, we splice it out of the array, and decrease the array size by one, returning true
+            bucket.splice(index, 1);
+            this.size--;
             return true;
         }
-        return false;
     }
 
+    //function to return the amount of keys in the bucket array, which we track with the size variable
     length() {
         return this.size;
     }
 
+    //replaces old bucket array with a new one, which starts back at initial capacity
+    //resets size to zero
     clear() {
-
+        this.buckets = Array.from({ length: 16 }, () => []);
+        this.size = 0;
     }
 
+    //function to return all keys in array
+    //create array of keys, iterate over every key in every bucket, and add it to the array and return it
     keys() {
+        let keyArray = [];
 
+        for(let bucket of this.buckets) {
+            for (let entry of bucket) {
+                keyArray.push(entry.key);
+            }
+        }
+        return keyArray;
     }
 
+    //same as keys(), but with values instead
     values() {
+        let valueArray = [];
 
+        for(let bucket of this.buckets) {
+            for(let entry of bucket) {
+                valueArray.push(entry.value);
+            }
+        }
+        return valueArray;
     }
 
+    //same as keys and values, but instead returning the pair of them together
     entries() {
+        let entryArray = [];
 
+        for(let bucket of this.buckets) {
+            for(let entry of bucket) {
+                entryArray.push([ entry.key, entry.value ]);
+            }
+        }
+        return entryArray;
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import { HashMap } from "./hashMap";
 
- const newMap = new HashMap(10, 0.75);
-console.log(newMap);
-newMap.set('Hello', 'World');
-newMap.length();
-newMap.has('Hello');
+//creating a test hashmap and adding pairs to it to see if it expands on its own (it does)
+let test = new HashMap();
+
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+test.set('hello', 'world');
+
+//testing functionality
+console.log(test);
+console.log(`Initial length: ${test.length()}`);
+console.log(`Remove hello?: ${test.remove('hello')}`);
+console.log(`Length after removing hello: ${test.length()}`);
+console.log(`Get value of key grape: ${test.get('grape')}`);
+console.log(`Has kite?: ${test.has('kite')}`);
+console.log(`Has melon?: ${test.has('melon')}`);
+console.log(`Array of keys: ${test.keys()}`);
+console.log(`Array of values: ${test.values()}`);
+console.log(`Array of key value pairs: ${test.entries()}`);
+test.clear()
+console.log(`After clearing, does it reset?`);
+console.log(test);
